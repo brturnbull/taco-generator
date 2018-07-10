d5a9e3c30d2b6156f7fa26ca990c2fb26d318128
Bridget <brturnbul@gmail.com>
2018-07-10 14:50:31 +1000

    seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -4,5 +4,32 @@ const { dbURI } = require('../config/environment');
 const Taco = require('../models/taco');
 
 mongoose.connect(dbURI, (err, db) => {
-  db.dropDatabase
+  db.dropDatabase();
+
+  Taco.create([{
+    condiment: 'Salsa',
+    mixin: 'Jalapeños',
+    seasoning: 'Aioli',
+    base_layer: 'Pulled Pork',
+    shell: 'Soft Corn Tortilla'
+  },{
+    condiment: 'Greek Yoghurt',
+    mixin: 'Charred Corn',
+    seasoning: 'Paprika Salt',
+    base_layer: 'Chipotle Chicken',
+    shell: 'Soft Flour Tortilla'
+  },{
+    condiment: 'Guacamole',
+    mixin: 'Corn Salsa',
+    seasoning: 'Turmeric Salt',
+    base_layer: 'Black Bean Chilli',
+    shell: 'Lettuce Leaf'
+  },{
+    condiment: 'A Cold Pacifico',
+    mixin: 'Mango Salsa',
+    seasoning: 'Jalapeno Aioli',
+    base_layer: 'Shrimp',
+    shell: 'Soft Flour Tortilla'
+  }])
+    .then(tacos => (console.log(`${tacos.length} delicious tacos created!`)))
 })
