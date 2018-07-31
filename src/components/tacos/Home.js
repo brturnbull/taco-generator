9a8c57ea209c4660a93155d5be7543a9cb4ab005
Bridget <brturnbul@gmail.com>
2018-07-31 21:08:12 +1000

    fix setstate bug

--- src/components/tacos/Home.js
+++ src/components/tacos/Home.js
@@ -3,8 +3,6 @@ import {
   Link
 } from 'react-router-dom';
 
-import RandomTaco from '../../components/tacos/Random';
-
 class Home extends React.Component {
 
   render() {
@@ -13,7 +11,7 @@ class Home extends React.Component {
         <img src="https://media.giphy.com/media/3o6ZtkmiFtpBvii6uQ/giphy.gif" data-reactid=".0.0" alt="dancing-taco" />
         <br />
         <Link to="/saved" className="button">Old Tacos</Link>
-        <Link to="/random" component={ RandomTaco } className="button">Random Taco</Link>
+        <Link to="/random" className="button">Random Taco</Link>
       </div>    );
   }
 
