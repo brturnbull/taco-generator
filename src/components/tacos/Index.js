68c2f934fecbffda7e748451287bacb1f070aabe
Bridget <brturnbul@gmail.com>
2018-09-26 16:20:44 +1000

    amended response for saved tacos page'

--- src/components/tacos/Index.js
+++ src/components/tacos/Index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 class Index extends React.Component {
+
+  state = {
+    data: ''
+  }
+
+  componentDidMount() {
+    axios.get('/api/tacos')
+      .then(console.log('here'))
+      .then(res => console.log(res.data));
+  }
 
   render() {
     return(
       <h1>saved tacos</h1>
+
+
     );
   }
 }
